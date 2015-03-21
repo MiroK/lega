6110216b936462efebf83f805a5303c1390e6e3b
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2015-03-21 20:14:10 +0100

    update

--- demo/poisson_2d_square_DP.py
+++ demo/poisson_2d_square_DP.py
@@ -17,12 +17,21 @@ def get_rhs(u):
     x, y = symbols('x, y')
     assert integrate(abs(u.subs(y, -1)), (x, -1, 1)) < 1E-15
     assert integrate(abs(u.subs(y, 1)), (x, -1, 1)) < 1E-15
-    print quad(lambdify(y, abs(u.subs(x, 0) - u.subs(x, 2*pi))), [-1, 1])
+    assert quad(lambdify(y, abs(u.subs(x, 0) - u.subs(x, 2*pi))), [-1, 1])
 
     # Right hand side if u is to be the solution
     f = -u.diff(x, 2) - u.diff(y, 2)
 
     return f
+
+
+def solve_poisson(f, n_fourier, n_shen):
+    '''
+    Solve the Poisson problem with highest frequency n_fourier and n_shen 
+    polynomials (that is n_shen+1 is the highest degree in that basis).
+    '''
+    # TODO
+    pass
 
 # -----------------------------------------------------------------------------
 
@@ -33,9 +42,11 @@ if __name__ == '__main__':
     x, y = symbols('x, y')
 
     k = 4
-    l = 4
+    l = 2
 
-    u = sin(k*pi*y)*y**2*cos(l*x)
+    # Idea is that sines are 'hard' to resolve for polynomials
+    # and polynomials are 'hard' to resolve for sines/cosines
+    u = sin(k*pi*y)*y*(x-pi)**(2*l)
     f = get_rhs(u)
 
     plot3d(u, (x, 0, 2*pi), (y, -1, 1))
